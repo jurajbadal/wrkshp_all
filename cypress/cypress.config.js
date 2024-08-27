7a2a223332df7a7a65c129be9f23a93c1dbd99a2
jurajbadal <juraj.badal@gmail.com>
2024-08-27 15:34:55 +0200

    #7

--- cypress/cypress.config.js
+++ cypress/cypress.config.js
@@ -5,5 +5,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    supportFile: 'cypress/support/e2e.js',
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
   },
 });
