0ed741753e35815f3c4745e68faefdc207936e90
jurajbadal <juraj.badal@gmail.com>
2024-09-10 17:04:10 +0200

    #7

--- accessibility/run-pa11y-safari.js
+++ accessibility/run-pa11y-safari.js
@@ -1,6 +1,6 @@
-const pa11y = require('pa11y');
-const puppeteer = require('puppeteer');
-const fs = require('fs').promises;
+const pa11y = import('pa11y');
+const puppeteer = import('puppeteer');
+const fs = import('fs').promises;
 
 const urls = [
   'http://37.27.17.198:8084/cs',
