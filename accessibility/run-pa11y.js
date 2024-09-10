ef3e104bf94854e7430de0da8854176448be6a37
jurajbadal <juraj.badal@gmail.com>
2024-09-10 14:21:06 +0200

    #5

--- accessibility/run-pa11y.js
+++ accessibility/run-pa11y.js
@@ -1,7 +1,9 @@
-import pa11y from 'pa11y';
-import htmlReporter from 'pa11y-reporter-html-plus';
-import { promises as fs } from 'fs';
+import { exec } from 'child_process';
+import { promisify } from 'util';
+import fs from 'fs/promises';
 import path from 'path';
+
+const execAsync = promisify(exec);
 
 const urls = [
   'http://37.27.17.198:8084/cs',
@@ -10,20 +12,14 @@ const urls = [
 ];
 
 async function runTests() {
-  let fullReport = '';
-  for (const url of urls) {
-    const result = await pa11y(url, {
-      standard: 'WCAG2AA',
-      reporter: 'html-plus'
-    });
-    const htmlResult = await htmlReporter.results(result);
-    fullReport += `<h2>Results for ${url}</h2>`;
-    fullReport += htmlResult;
-  }
   const reportDir = 'accessibility';
   await fs.mkdir(reportDir, { recursive: true });
-  await fs.writeFile(path.join(reportDir, 'plus-report.html'), fullReport);
+
+  for (const url of urls) {
+    const command = `npx pa11y --reporter=html-plus ${url}`;
+    const { stdout } = await execAsync(command);
+    await fs.appendFile(path.join(reportDir, 'plus-report.html'), stdout);
+  }
 }
 
 runTests();
-
